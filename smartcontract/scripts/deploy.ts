7aef72b778c62ed54fc9b636adf6df7267e86dc4
Aryan <aryan.pathania2003@gmail.com>
2022-11-06 13:04:29 +0530

    ✨ Add deploy script

--- smartcontract/scripts/deploy.ts
+++ smartcontract/scripts/deploy.ts
@@ -1,23 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const DecentralizedWarrantyContract = await ethers.getContractFactory("DecentralizedWarranty");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const DecentralizedWarranty = await DecentralizedWarrantyContract.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  await DecentralizedWarranty.deployed();
+  console.log(`DecentralizedWarranty contract address: ${DecentralizedWarranty.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
