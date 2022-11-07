9bbcb166639494b6c6c9d23e264ba08a686b68d3
Aryan <aryan.pathania2003@gmail.com>
2022-11-07 19:48:09 +0530

    ✨ List all NFTs owned by `address`

---
+++ frontend/pages/api/list.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { listNFTs } from '../../utils/NFThelpers';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+  const { address } = req.body;
+  const list: string[] = await listNFTs(address);
+  res.status(200).json(list);
+}
