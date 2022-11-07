4b0df66a39972914935619bf6b6b7819bc5e315f
Aryan <aryan.pathania2003@gmail.com>
2022-11-07 12:56:21 +0530

    ✨ Add helper function to list all NFTs

---
+++ frontend/utils/NFThelpers.ts
@@ -0,0 +1,22 @@
+import { Contract, ContractWithSigner } from "./ether";
+
+function mintNFT(
+  productID : string, 
+  serialNo : string, 
+  ownerWallet : string, 
+  linkIPFS : string
+  ) {
+  return ContractWithSigner.mintNFT(productID + serialNo, ownerWallet, linkIPFS);
+}
+
+async function listNFTs(address : string) {
+  const number = await Contract.balanceOf(address);
+  const listNFTs : string[] = [];
+  for (let i = 0; i < number; i++) {
+    const tokenID = await Contract.tokenOfOwnerByIndex(address, i);
+    const tokenURI = await Contract.tokenURI(tokenID);
+    listNFTs.push(tokenURI);
+  }
+}
+
+export { mintNFT, listNFTs };
