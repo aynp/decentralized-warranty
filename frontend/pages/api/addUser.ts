8eb217589ea76241119f3a4c5feaa94ef6fcac69
Adarsh Singh <thakurluckysinghbrh@gmail.com>
2022-11-07 21:39:38 +0530

    feat: add warranty and ipfs to product.ts

--- frontend/pages/api/addUser.ts
+++ frontend/pages/api/addUser.ts
@@ -10,7 +10,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'POST') return;
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method not allowed' });
+    return;
+  }
 
   const { userID, isWallet } = req.body as Body;
 
