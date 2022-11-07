ba7930c2e16777aa2cd4aea31c8084f8cf98fdb3
Aryan <aryan.pathania2003@gmail.com>
2022-11-07 18:48:24 +0530

    ✨ Add functions

--- frontend/utils/NFThelpers.ts
+++ frontend/utils/NFThelpers.ts
@@ -18,25 +18,25 @@ async function mintNFT(
   }
 }
 
-// async function listNFTs(address: string) {
-//   const number = await Contract.balanceOf(address);
-//   const listNFTs: string[] = [];
-//   for (let i = 0; i < number; i++) {
-//     const tokenID = await Contract.tokenOfOwnerByIndex(address, i);
-//     const tokenURI = await Contract.tokenURI(tokenID);
-//     listNFTs.push(tokenURI);
-//   }
-//   return listNFTs;
-// }
+async function listNFTs(address: string) {
+  const number = await Contract.balanceOf(address);
+  const listNFTs: string[] = [];
+  for (let i = 0; i < number; i++) {
+    const tokenID = await Contract.tokenOfOwnerByIndex(address, i);
+    const tokenURI = await Contract.tokenURI(tokenID);
+    listNFTs.push(tokenURI);
+  }
+  return listNFTs;
+}
 
-// async function getTokenURIs(tokenIDs: string[]) {
-//   const tokenURIs = [];
-//   for (let i = 0; i < tokenIDs.length; i++) {
-//     const tokenURI = Contract.tokenURI(tokenIDs[i]);
-//     tokenURIs.push(tokenURI);
-//   }
+async function getTokenURIs(tokenIDs: string[]) {
+  const tokenURIs = [];
+  for (let i = 0; i < tokenIDs.length; i++) {
+    const tokenURI = Contract.tokenURI(tokenIDs[i]);
+    tokenURIs.push(tokenURI);
+  }
 
-//   return await Promise.all(tokenURIs);
-// }
+  return await Promise.all(tokenURIs);
+}
 
-export { mintNFT };
+export { mintNFT, listNFTs, getTokenURIs };
