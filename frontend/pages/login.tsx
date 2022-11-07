43dab8eda7131cf8134bd443485503e90c4f800f
Aryan <aryan.pathania2003@gmail.com>
2022-11-07 16:12:36 +0530

    💄 Update login page

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import {
   useAddress,
   useUser,
@@ -14,48 +16,59 @@ export default function Example() {
   const { user } = useUser();
 
   return (
-    <>
-      <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            Soci
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in here
-              </h1>
-
-              <button
-                type="submit"
-                className="text-lg p-4 shadow-sm text-white border-0 focus:outline-none rounded-md bg-[#000000] hover:bg-[#0a3650] flex items-center justify-center w-full">
-                Sign in with Google
-              </button>
-              <a href="/">
-                <button
-                  onClick={connect}
-                  type="submit"
-                  className="text-lg p-4 shadow-sm text-white border-0 focus:outline-none rounded-md bg-[#000000] hover:bg-[#0a3650] flex items-center justify-center w-full">
-                  Sign in with Metamusk
-                </button>
-              </a>
-            </div>
-            {/* <div>
-              {address ? (
-                <>
-                  <button onClick={() => login()}>Login with Wallet</button>
-                  <button onClick={logout}>Logout</button>
-                  <pre>User: {JSON.stringify(address || null)}</pre>
-                </>
-              ) : (
-                <button onClick={connect}>Connect Wallet</button>
-              )}
-            </div> */}
-          </div>
-        </div>
-      </section>
-    </>
+    <div className="grid place-items-center h-screen">
+      <div className="p-4 w-full max-w-sm rounded-lg border shadow-md sm:p-6 border-black">
+        <h5 className="mb-3 text-base font-semibold md:text-xl text-white text-center">
+          Login
+        </h5>
+        <ul className="my-4 space-y-3">
+          <li>
+            <a
+              href="#"
+              className="flex items-center p-3 text-base font-bold text-white rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+              <Image
+                alt="Google"
+                width={24}
+                height={24}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/240px-Google_%22G%22_Logo.svg.png"
+              />
+              <span className="flex-1 ml-3 whitespace-nowrap">Google</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center p-3 text-base font-bold text-white rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+              <Image
+                alt="Metamask"
+                width={24}
+                height={24}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/768px-MetaMask_Fox.svg.png"
+              />
+              <span className="flex-1 ml-3 whitespace-nowrap">MetaMask</span>
+              <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                Recommended
+              </span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center p-3 text-base font-bold text-white rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+              <Image
+                alt="Solana"
+                width={24}
+                height={24}
+                src="https://3632261023-files.gitbook.io/~/files/v0/b/gitbook-legacy-files/o/spaces%2F-MVOiF6Zqit57q_hxJYp%2Favatar-1615495356537.png?generation=1615495356841399&alt=media"
+              />
+              <span className="flex-1 ml-3 whitespace-nowrap">Phantom</span>
+              <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                Recommended
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
