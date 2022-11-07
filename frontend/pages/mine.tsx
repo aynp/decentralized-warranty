a40b45af8a2cc7a624b30f54818950d3622cb011
Aryan <aryan.pathania2003@gmail.com>
2022-11-07 21:59:18 +0530

    🐛 Fix build issues

--- frontend/pages/mine.tsx
+++ frontend/pages/mine.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { products } from '../data/product.js';
+import { products } from '../data/product';
 import Layout from '../components/UI/Layout';
 import ProductCard from '../components/ProductCard';
 
@@ -7,7 +7,9 @@ export default function Store() {
   console.log(products);
   return (
     <Layout>
-      <p className="mt-16 mb-16 text-center font-semibold text-6xl">My orders</p>
+      <p className="mt-16 mb-16 text-center font-semibold text-6xl">
+        My orders
+      </p>
       <div className="flex flex-wrap mt-10 mb-10 justify-center">
         {products.map((card) => {
           return <ProductCard key={card.id} card={card} />;
